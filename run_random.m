d794dcee6ea59e4e6c59069576536e11e5e58200
PoliMala <andrea.malatesta@mail.polimi.it>
2022-09-22 15:38:02 +0000

    sample simulation run starting from a random initial condition

---
+++ run_random.m
@@ -0,0 +1,75 @@
+function run_random(varargin)
+%%                  run_random( optName1, optVal1, ... )
+
+addpath(genpath('functions'))
+
+% choose solution field names, these will be exported in the caller
+% workspace
+sol = struct();
+optName  = "options";
+EOSname  = "equations";
+solName  = "solution";
+timeName = "time";
+
+% the range of data options can be found through 
+%   help setOptions
+dataRange = [1,6];
+simRange  = [7,8];
+
+%% Generate Equation Of State
+
+% generate options and save EOS
+opt             = setOptions(varargin);
+[EOS, K, n]     = genEOS(opt,dataRange);
+
+%% Simulate random initial condition
+
+% write simulation option in the workspace
+writeOptions(opt,simRange)
+maxDeg_rad = pi/180*maxDeg;
+
+% generate random initial condition with -degMax < angle (X(2))  < degMax
+% and zero velocities, at the origin (X(1) = 0)
+X0      = zeros([n,1]);
+circ    = 0.3;
+X0(2:n/2)  = circ*maxDeg_rad*rand([n/2-1,1]) + (1-circ)*maxDeg_rad;
+
+% simulate 
+[t, tmp] = ode113(EOS, [0,tFinal], X0);
+Xh = tmp'; clear tmp
+
+%% Plot some result
+
+% write data for postprocessing
+writeOptions(opt,dataRange);
+
+% display animation
+animateSolution(t, Xh, l, n)
+
+% Control force
+u = (K*Xh)/(M+m)/g;
+figure
+plot(t,u,'LineWidth',1.4)
+xlabel('Time [s]','Interpreter','latex')
+ylabel('Force [g-units]','Interpreter','latex')
+title('\textbf{Force applied by the controller}','Interpreter','latex')
+
+% cart velocity
+figure
+plot(t,Xh(3,:),'LineWidth',1.4)
+xlabel('Time [$s$]','Interpreter','latex')
+ylabel('Velocity along x direction [$\frac{m}{s}$]','Interpreter','latex')
+title('\textbf{Cart Velocity}','Interpreter','latex')
+
+%% Export variables in the caller workspace
+
+sol.(optName)   = opt;
+sol.(EOSname)   = EOS;
+sol.(timeName)  = t;
+sol.(solName)   = Xh;
+
+% load solution fields
+varaibles = fieldnames(sol);
+for ii = 1:length(varaibles)
+    assignin('caller', varaibles{ii}, sol.(varaibles{ii}))
+end
